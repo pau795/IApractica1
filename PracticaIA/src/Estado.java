4a0d2869e45563dd9d1a8e33c904ebdb68a04689
jgame95 <jbg1@hotmail.es>
2017-03-23 15:13:15 +0100

    Update Estado.java
    He añadido la funcion //intercambia2(int fuente, int destino)// y tambien añadi dos hashmaps

--- PracticaIA/src/Estado.java
+++ PracticaIA/src/Estado.java
@@ -11,6 +11,11 @@ public class Estado {
 	static CentrosDatos d;
 	HashMap<Integer, Integer> GDA; 	//Grafo Dirigido Aciclico. Relaciona identificadores <Sensor, Destino>
 									// Si Destino >= 0, Destino Codifica un sensor. Sino, codifica un Centro
+	/*HashMap para saber cuantos sensores tienen conectados cada sensor*/
+	HashMap<Integer, Integer> SconnectS;
+	/*HashMap para saber cuantos sensores tienen conectados cada centro*/
+	HashMap<Integer, Integer> SconnectC;
+	
 	
 	public Estado(HashMap<Integer, Integer> g, Sensores s, CentrosDatos d){
 		Estado.s=s;
@@ -21,11 +26,20 @@ public class Estado {
 		}
 	}
 	
-	public Estado (int ns, int nc,int seed){		//Propuesta de solucion inicial.
-		s = new Sensores(ns, seed);					//Se conecta cada sensor i a un centro c respecto i mod num_centros
-		d = new CentrosDatos(nc, seed);				// 
-		for(int i=0; i<ns; ++i){
-			GDA.put(i, -(i%nc)-1);
+	public Estado (int ns, int nc,int seed){		
+		s = new Sensores(ns, seed);
+		d = new CentrosDatos(nc, seed);
+		/*Inicializamos los hashmap indicando que no tienen conexión alguna*/
+		for (int i = 0; i < ns; ++i) {
+			SconnectS.put(i, 0);
+		}
+		for (int i = 0 ; i < nc; ++i) {
+			SconnectC.put(i, 0);
+		}
+		for(int i=0; i<ns; ++i){					//Propuesta de solucion inicial.
+			GDA.put(i, -(i%nc)-1);				//Se conecta cada sensor i a un centro c respecto i mod num_centros
+			/*Modificaremos las conexiones que tienen inicialmente*/
+			SconnectC.put(-(i%nc)-1, (SconnectC.get(-(i%nc)-1))+1);
 		}
 	}
 	
@@ -60,7 +74,24 @@ public class Estado {
 		GDA.put(fuente, destino);
 	}
 	
-	
+	void intercambia2(int fuente, int destino) {
+		
+		/*Compruebo si me quiero conectar a un sensor
+		 * y que el sensor al que me quiero conectar tenga menos de 3 conexiones*/
+		if (destino >= 0 && SconnectS.get(destino) < 3) {
+			GDA.put(fuente, destino);
+			/*Modifico el numero de conexiones que tiene el destino*/
+			SconnectS.put(destino, (SconnectS.get(destino))+1);
+		}
+		
+		/*Compruebo si me quiero conectar a un centro
+		 * y que el centro al que me quiero conectar tenga menos de 25 conexiones*/
+		else if (destino < 0 && SconnectC.get(destino) < 25) {
+			GDA.put(fuente, destino);
+			/*Modifico el numero de conexiones que tiene el destino*/
+			SconnectC.put(destino, (SconnectC.get(destino))+1);
+		}
+	}
 }
 
 
